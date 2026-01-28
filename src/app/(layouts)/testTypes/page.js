ef2d670f15a06d0e5b03ec8b71e96b17c363ac0d
marooncoucal <marooncoucal@gmail.com>
2026-01-28 16:54:50 +0300

    import page js correction

--- src/app/(layouts)/testTypes/page.js
+++ src/app/(layouts)/testTypes/page.js
@@ -1,10 +1,3 @@
-import Link from "next/link"
-import PodborPage from "./podbor/page"
-import PuzzlePage from "./puzzle/page"
-import QuizPage from "./quiz/page"
-import TruePage from "./true/page"
-import True from "@/app/_components/games/True"
-
 export default function TestTypesContainer() {
   return (
     <div className="entryTest-js w-full h-full flex flex-col gap-5 items-center">
